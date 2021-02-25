e2bd196dc6f79d1c585fd88245ada5f1da98c470
IceCreamIsYUMMY <55368932+PancakeTY@users.noreply.github.com>
2021-02-25 14:50:57 +0800

    Update storm-scheduler.sql

--- storm-scheduler.sql
+++ storm-scheduler.sql
@@ -107,6 +107,21 @@ CREATE TABLE `traffic` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
+DROP TABLE IF EXISTS `cpu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cpu` (
+  `topology_id` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `thread_id` int(11) NOT NULL,
+  `task_id` int(11) NOT NULL,
+  `interval` int(11) NOT NULL,
+  `cputime` int(11) NOT NULL,
+  UNIQUE KEY `task_id_UNIQUE` (`task_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
